5a5f52c996f2887eb51c35e658e5d6dad2ff5e1f
Muneebshahid570 <168467556+Muneebshahid570@users.noreply.github.com>
2024-05-24 12:00:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+let m = 40;
+let n = 80;
+let o = 10;
+console.log(m + n + o);
+console.log(m - m - 0);
+console.log(n * m * o);
+console.log(n / m / o);
+console.log(n % m % o);
+//boolean data type;
+console.log(true);
+console.log(false);
+console.log(2 === 2);
